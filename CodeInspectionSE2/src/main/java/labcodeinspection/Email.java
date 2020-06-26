d4f8c70cd2962bc77cb52ab8a7aa1ca8d0d8f819
hmaticur@espol.edu.ec <hmaticur@espol.edu.ec>
2020-06-26 15:45:19 -0500

    Reports and corrected code without violations

--- CodeInspectionSE2/src/main/java/labcodeinspection/Email.java
+++ CodeInspectionSE2/src/main/java/labcodeinspection/Email.java
@@ -1,51 +1,115 @@
 package labcodeinspection;
 
+import java.util.Locale;
+
 public class Email {
 
-	private String m_firstName;
-	private String m_lastName;
-	private String password = null;
+	private String mfirstName;
+	private String mlastName;
+	private String password;
 	private String department;
-	private int defaultpasswordLength = 8;
+	private int defpasswLength = 8;
 	private String email;
 
-	public Email(String firstName, String lastName) {
-		this.m_firstName = firstName;
-		this.m_lastName = lastName;
+	/**This constructor will save the essential information to recognize the use.
+	 * @param firstName and @param lastName we allow to know the full name the user
+	 */
+	public Email(final String firstName, final String lastName) {
+		this.setMfirstName(firstName);
+		this.setMlastName(lastName);
 	}
 
+	/**This method show of information save in the class
+	 * @param void
+	 */
 	public void showInfo() {
-		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
-		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
+		System.out.println("\nFIRST NAME= " + getMfirstName() + "\nLAST NAME= " + getMlastName());
+		System.out.println("DEPARMENT= " + getDepartment() + "\nEMAIL= " + getEmail() + "\nPASSWORD= " + getPassword());
 	}
 
-	public void setDeparment(int depChoice) {
+	/**This method allow to save the deparment in the class with a number choosed
+	 * @param depChoise
+	 */
+	public void setDeparment(final int depChoice) {
 		switch (depChoice) {
 		case 1:
-			this.department = "sales";
+			this.setDepartment("sales");
 			break;
 		case 2:
-			this.department = "dev";
+			this.setDepartment("dev");
 			break;
-		case 3:
-			this.department = "acct";
+		default:
+			this.setDepartment("acct");
 			break;
+		
 		}
 	}
 
-	private String randomPassword(int length) {
-		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
+	private String randomPassword(final int length) {
+		final String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
 		char[] password = new char[length];
 		for (int i = 0; i < length; i++) {
-			int rand = (int) (Math.random() * set.length());
+			final int rand = (int) (Math.random() * set.length());
 			password[i] = set.charAt(rand);
 		}
 		return new String(password);
 	}
 
+	/**This method allow generate of email in base of the information saved.
+	 */
 	public void generateEmail() {
-		this.password = this.randomPassword(this.defaultpasswordLength);
-		this.email = this.m_firstName.toLowerCase() + this.m_lastName.toLowerCase() + "@" + this.department
-				+ ".espol.edu.ec";
+		this.setPassword(this.randomPassword(this.getDefpasswLength()));
+		this.setEmail(this.getMfirstName().toLowerCase(Locale.getDefault()) + this.getMlastName().toLowerCase(Locale.getDefault()) + "@" + this.getDepartment()
+				+ ".espol.edu.ec");
 	}
+
+	public String getMfirstName() {
+		return mfirstName;
+	}
+
+	public void setMfirstName(final String mfirstName) {
+		this.mfirstName = mfirstName;
+	}
+
+	public String getMlastName() {
+		return mlastName;
+	}
+
+	public void setMlastName(final String mlastName) {
+		this.mlastName = mlastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(final String department) {
+		this.department = department;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	public int getDefpasswLength() {
+		return defpasswLength;
+	}
+
+	public void setDefpasswLength(final int defpasswLength) {
+		this.defpasswLength = defpasswLength;
+	}
+	
+	
 }
--- CodeInspectionSE2/src/main/java/labcodeinspection/EmailApp.java
+++ CodeInspectionSE2/src/main/java/labcodeinspection/EmailApp.java
@@ -2,23 +2,32 @@ package labcodeinspection;
 
 import java.util.Scanner;
 
+
 public class EmailApp {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	private EmailApp() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * The method will register the information of the new user.
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		final Scanner reader = new Scanner(System.in);
 
 		System.out.print("Enter your first name: ");
-		String firstName = sc.nextLine();
+		final String firstName = reader.nextLine();
 
 		System.out.print("Enter your last name: ");
-		String lastName = sc.nextLine();
+		final String lastName = reader.nextLine();
 
 		System.out.print("\nDEPARTMENT CODE\n1. for sales\n2. for Development\n3. for accounting\nEnter code: ");
 
-		int depChoice = sc.nextInt();
-		sc.close();
+		final int depChoice = reader.nextInt();
+		reader.close();
 
-		Email email = new Email(firstName, lastName);
+		final Email email = new Email(firstName, lastName);
 		email.setDeparment(depChoice);
 		email.generateEmail();
 		email.showInfo();
